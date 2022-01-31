c994b4e291bf722a955560ab24e9b4101e92cc83
troymeeker <troymeeker1@gmail.com>
2022-01-31 12:33:58 -0800

    commit tree 37fda123fd427da916a905c7d41c2ccd09c5c79c

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,9 @@
 require_relative "../config/environment.rb"
 
 class Student
+  def initialize :name, :grade, :id = 'nil'
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  end
 
 
 end
